3c89ccfb7ecf086d90c547300712e0d78ee75c3c
Timothy Farkas <timothyfarkas@apache.org>
2021-01-08 22:29:20 -0800

    - Have simple script to plot trades

--- plot_trades.py
+++ plot_trades.py
@@ -5,9 +5,21 @@ import plotly.express as px
 @click.command()
 @click.argument('source')
 def main(source):
-    df = pd.read_csv(source)
-    df['timestampNano'] = df['timestampNano'].transform(lambda ts: pd.to_datetime(ts, unit='ns', origin='unix'))
-    fig = px.scatter(df, x="timestampNano", y="price")
+    df = pd.read_csv(source).drop(columns = [
+        'originalTradeID',
+        'exchangeID',
+        'tradeID',
+        'correctionIndicator',
+        'reportingFacilityID',
+        'quoteTimestampNano',
+        'sequenceNo',
+        'tape'])
+    df = df.set_index(['timestampNano'])
+    df.index = pd.to_datetime(df.index, unit='ns', origin='unix')
+    df['totalPrice'] = df['price'] * df['size']
+    sdf = df.groupby(pd.Grouper(freq='1Min', offset=0)).sum()
+    sdf['avPrice'] = sdf['totalPrice'] / sdf['size']
+    fig = px.line(sdf, x=sdf.index, y='avPrice')
     fig.show()
 
 if __name__ == "__main__":
