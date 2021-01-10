901946654155b3e45d1c8d6322af5feb30b7a959
Timothy Farkas <timothyfarkas@apache.org>
2021-01-09 21:27:26 -0800

    - plot min and max lines

--- plot_trades.py
+++ plot_trades.py
@@ -1,6 +1,7 @@
 import click
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objs as go
+import numpy as np
 
 @click.command()
 @click.argument('source')
@@ -16,10 +17,41 @@ def main(source):
         'tape'])
     df = df.set_index(['timestampNano'])
     df.index = pd.to_datetime(df.index, unit='ns', origin='unix')
-    df['totalPrice'] = df['price'] * df['size']
-    sdf = df.groupby(pd.Grouper(freq='1Min', offset=0)).sum()
-    sdf['avPrice'] = sdf['totalPrice'] / sdf['size']
-    fig = px.line(sdf, x=sdf.index, y='avPrice')
+    df = df.tz_localize("UTC")
+    df = df.tz_convert("US/Pacific")
+    df = df.between_time('6:00', '15:00')
+
+    adf = df.groupby(pd.Grouper(freq='1Min', offset=0)).agg({'price': [np.mean, np.std, np.min, np.max]})
+    adf.columns = adf.columns.map('_'.join).str.strip()
+    print(adf.head())
+
+    fig = go.Figure([
+        go.Scatter(
+            name='Average',
+            x=adf.index,
+            y=adf['price_mean'],
+            mode='lines',
+            line=dict(color='rgb(31, 119, 180)')),
+        go.Scatter(
+            name='min',
+            x=adf.index,
+            y=adf['price_amin'],
+            marker=dict(color="#411"),
+            line=dict(width=1),
+            mode='lines'),
+        go.Scatter(
+            name='max',
+            x=adf.index,
+            y=adf['price_amax'],
+            marker=dict(color="#141"),
+            line=dict(width=1),
+            mode='lines')
+    ])
+    fig.update_layout(
+        yaxis_title='Price',
+        title='Error bars, min, and max',
+        hovermode="x"
+    )
     fig.show()
 
 if __name__ == "__main__":
